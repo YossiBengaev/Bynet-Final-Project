f8bec3dbb62f41e6e8da54343499b7e939b62c51
YossiBengaev <yossibenga@gmail.com>
2022-10-01 13:26:47 +0300

    Adding flask app

---
+++ flask_app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+import pandas as pd
+import os
+import mysql_connection
+
+app = Flask(__name__)
+
+df = mysql_connection.connection_attendace_csv_to_mysql()
+
+@app.route('/')
+@app.route('/home')
+def html_table():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/attendance')
+def attendance_table():
+    return render_template('dataframe.html',  tables=[df.to_html(classes='data', header="true")])
+
+
+@app.route('/contact')
+def contact_me():
+    return render_template('contact.html')
+
+
+#if __name__ == "__main__":
+    #app.run(debug=True)
+    
+app.run(host='0.0.0.0', port=5000)
+    
