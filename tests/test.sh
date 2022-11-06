af3675d837021341ed2328ce2ec9beba60a7ffd9
Yossi Bengaev <82327346+YossiBengaev@users.noreply.github.com>
2022-11-06 17:47:16 +0200

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+HTTP=`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://127.0.0.1:8080"`
+echo $HTTP
+if [ "$HTTP" == "200" ]; 
+then echo "Test succedded"
+else
+  echo "Test failed"
+fi
