3e0b6ea6cc434f22f9e2c71975349d961e06832f
Yossi Bengaev <82327346+YossiBengaev@users.noreply.github.com>
2022-11-07 10:02:57 +0200

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-HTTP=`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://127.0.0.1:8080"`
-echo $HTTP
-if [ "$HTTP" == "200" ]; 
-then echo "Test succedded"
-else
-  echo "Test failed"
-fi
+echo "Hello " ; hostname 
+
+#HTTP=`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://127.0.0.1:8080"`
+#echo $HTTP
+#if [ "$HTTP" == "200" ]; 
+#then echo "Test succedded"
+#else
+  #echo "Test failed"
+#fi
