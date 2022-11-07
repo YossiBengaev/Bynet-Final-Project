72f3f3682705f060710957089ccbb5e759157593
Yossi Bengaev <82327346+YossiBengaev@users.noreply.github.com>
2022-11-07 17:48:45 +0200

    Update and rename cleanup.sh to cleanup_test.sh

---
+++ cleanup_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+MIN=$1
+SEC=$((MIN * 60))
+echo "Going to sleep for" $SEC "seconds and then removing all the docker stuff!"
+sleep $SEC
+
+ssh -o StrictHostKeyChecking=no ec2-user@$test 'docker-compose down && docker container prune -f && docker image prune -f && docker volume prune -f'
+
+#docker-compose down
+#docker container prune -f 
+#docker image prune -f
+#docker volume prune -f
