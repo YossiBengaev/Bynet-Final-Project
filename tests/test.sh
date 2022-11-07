758c02c1d481d364723649729467566e5e49ec09
Yossi Bengaev <82327346+YossiBengaev@users.noreply.github.com>
2022-11-07 14:05:45 +0200

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-echo "Hello " ; hostname 
-
-#curl -Is http://127.0.0.1:5000 | head -n 1
-HTTP=`curl --write-out "%{http_code}\n" --silent "http://127.0.0.1:5000"`
-echo $HTTP
-if [ "$HTTP" == "200" ]; 
-then echo "Test succedded"
-else
-  echo "Test failed"
-fi
+curl -Is http://127.0.0.1:5000 | head -n 1
