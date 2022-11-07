c68250d2331e6bad697c0ed52698e3a570aa0507
Yossi Bengaev <82327346+YossiBengaev@users.noreply.github.com>
2022-11-07 14:32:13 +0200

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl -Is http://127.0.0.1:5000 | head -n 1
+RESPONSE=$(curl -Is http://127.0.0.1:5000 | head -n 1)
+if [ "$RESPONSE"="HTTP/1.1 200 OK" ]
+  then echo "Http response is 200 it is mean the app is running and you are good to go!!"
+ else
+  echo "Not so fast! you have code status " + $RESPONSE + " please try again... " ; exit 1 
