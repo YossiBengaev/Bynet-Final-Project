a82566c2630a5c96a95ab6d5218570d7815e544a
Yossi Bengaev <82327346+YossiBengaev@users.noreply.github.com>
2022-11-07 17:49:27 +0200

    Create cleanup_production.sh

---
+++ cleanup_production.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MIN=$1
+SEC=$((MIN * 60))
+echo "Going to sleep for" $SEC "seconds and then removing all the docker stuff!"
+sleep $SEC
+
+ssh -o StrictHostKeyChecking=no ec2-user@$production 'docker-compose down && docker container prune -f && docker image prune -f && docker volume prune -f'
