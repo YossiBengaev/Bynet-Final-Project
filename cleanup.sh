2936140ee6dafb248c5ca6a23baeb2eafb658c23
Yossi Bengaev <82327346+YossiBengaev@users.noreply.github.com>
2022-11-07 15:30:03 +0200

    add removing docker stuff

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Going to sleep now "
-secs=$((1 * 60))
+echo "Going to sleep now and the removing all the docker stuff!"
+secs=$(($1 * 60))
 while [ $secs -gt 0 ]; do
    echo -ne "$secs\033[0K\r"
    sleep 1
    : $((secs--))
 done
 docker-compose down
+docker container prune 
+docker image prune 
+docker volume prune 
